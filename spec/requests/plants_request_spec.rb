81baea99d9285cd86edda9f7405e3d5bcd596e4c
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2024-01-02 12:13:29 +0900

    デプロイの為、indexアクション設定

---
+++ spec/requests/plants_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Plants", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/plants/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
